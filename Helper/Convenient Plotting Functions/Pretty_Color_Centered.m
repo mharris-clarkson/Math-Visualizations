9ee68c962b6947260897ef5ed2eea606aa84a25e
mharris-clarkson <mwharris@clarkson.edu>
2026-05-03 10:32:10 -0400

    Added colorbar option to Pretty_Color_Centered

--- Helper/Convenient Plotting Functions/Pretty_Color_Centered.m
+++ Helper/Convenient Plotting Functions/Pretty_Color_Centered.m
@@ -3,7 +3,7 @@
 %   current_axis  - axis to apply to
 %   current_axis  - colour data to balance out
 %   cb            - Returned colorbar
-function cb = Pretty_Color_Centered(current_axis,cdata)
+function cb = Pretty_Color_Centered(current_axis,cdata,options)
 % Pretty_Color_Centered is a helper function for changing to a divergence 
 % frees color map (provided by Kristen Thyng's cmocean function, centering 
 % it, adding a color bar and shifting the colorbar for better 3D function 
@@ -27,17 +27,21 @@ function cb = Pretty_Color_Centered(current_axis,cdata)
 %
 % This function was written by Dr Matthew Harris an assistant professor at
 % Clarkson university for visually teaching MA 231: Calculus 3.
-
+arguments 
+current_axis
+cdata
+options.colorbar = 'on'
+end
 
 % Use the balance colormap
 colormap(current_axis, cmocean('balance'));
 % Center the color axis
 clim(current_axis,max(abs(cdata(:)))*[-1,1]);
+
 % Place a colorbar and adjust its horizontal position
+if strcmp(options.colorbar, 'on')
 cb = colorbar(current_axis,'Location', 'eastoutside');
 ax_pos = current_axis.Position;
 current_axis.Position = ax_pos;
-
-
-
+end
 end
